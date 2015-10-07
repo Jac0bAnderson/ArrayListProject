7ee15212640fe7e61c885bc9bed9f1c0816089b9
Jac0bAnderson <jand6944@740s-pl-204.local>
2015-10-07 09:36:09 -0600

    arraylist
    a project containing array lists

---
+++ src/list/controller/ArrayListRunner.java
@@ -0,0 +1,12 @@
+package list.controller;
+
+import lists.model.ArrayListController;
+
+public class ArrayListRunner
+{
+ public static void main(String [] args)
+ {
+	 ArrayListController ListController = new ArrayListController();
+	 ListController.start();
+ }
+}
---
+++ src/lists/model/ArrayListController.java
@@ -0,0 +1,9 @@
+package lists.model;
+
+public class ArrayListController
+{
+ public void start()
+ {
+	 
+ }
+}
