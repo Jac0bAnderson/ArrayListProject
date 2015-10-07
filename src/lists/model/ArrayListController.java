f145d0464b53619afc06d73e5a6297cabda6f4b0
Jac0bAnderson <jand6944@740s-pl-204.local>
2015-10-07 09:56:58 -0600

    arraylist
    a window shows the spot of strings.

--- src/lists/model/ArrayListController.java
+++ src/lists/model/ArrayListController.java
@@ -1,9 +1,31 @@
 package lists.model;
 
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 public class ArrayListController
 {
+	private ArrayList<String> firstWords;
+	
+	public  ArrayListController() //default constructor 
+	{
+		firstWords = new ArrayList<String>();
+	}
+	
  public void start()
  {
-	 
+	firstWords.add("asdfjkl;sadjkfjl"); 
+	firstWords.add("babababbabnadadaabbab");
+	firstWords.add("( ͡° ͜ʖ ͡°)");
+	showList();
  }
+ 
+ private void showList()
+ {
+	 for(int spot =0; spot < firstWords.size();spot++)
+		 JOptionPane.showMessageDialog(null, firstWords.get(spot)+ "is at spot#" + spot);
+	 JOptionPane.showMessageDialog(null, "( ͡° ͜ʖ ͡°) hey lenny!");
+ }
+ 
+ 
 }
